6c76f2cb84d939ba637d168b768dc49ddfa7b0d3
Dylan Nicolini <dylan.nicolini@gmail.com>
2019-06-14 15:52:41 -0400

    committing lab

---
+++ observable-service/src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  cartItemsCount = 0;
+  shoppingCartSub: Subscription;
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  ngOnInit() {
+    this.shoppingCartSub = this.shoppingCartService.shoppingCartChanged$.subscribe(val => {
+      this.cartItemsCount = val;
+    });
+  }
+
+  ngOnDestroy() {
+    this.shoppingCartSub.unsubscribe();
+  }
+
+}
